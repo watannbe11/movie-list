b07c4961427224756d2f634bf7a97c8f1024de74
yuichiro watanabe <yuuichiro1139@gmail.com>
2020-04-13 18:22:31 +0900

    add

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,14 +20,15 @@ movie = []
 num = 1
 
 html_doc.search("tbody tr")[1..8].each do |element|
-  movie[num] = Movie.create!(
-    title: element.search("td[2]").text.strip,
-    year: element.search("td[7]").text.strip.to_i
-    )
 
   url2 = "http://www.omdbapi.com/?apikey=fb36b2dd&t=#{element.search("td[2]").text.strip}"
   html_file2 = open(url2).read
   movie_api = JSON.parse(html_file2)
+  movie[num] = Movie.create!(
+    title: element.search("td[2]").text.strip,
+    year: element.search("td[7]").text.strip.to_i,
+    plot: movie_api["Plot"]
+    )
   file = URI.open("#{movie_api["Poster"]}")
   movie[num].photo.attach(io: file, filename: 'parasaite.jpg', content_type: 'image/jpg')
   num += 1
